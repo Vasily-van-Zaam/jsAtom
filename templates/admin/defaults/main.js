36910128e58515d0aee6e23017a612cd2852f532
Vasiliy <vasiliy.vanzaam@gmail.com>
2015-10-02 00:00:00 +0300

    0.0.0...

---
+++ templates/admin/defaults/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Vasiliy on 30.09.2015.
+ */
+(function (){
+    var defaults = {};
+
+    defaults.action = function (){
+        return "template admin default"
+
+    };
+
+    jsAtom.templates.admin.defaults = defaults
+
+}).call(this);
---
+++ templates/content/defaults/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Vasiliy on 30.09.2015.
+ */
+(function (){
+    var defaults = {};
+
+    defaults.action = function (){
+        return "template content defaults"
+
+    };
+
+    jsAtom.templates.content.defaults = defaults
+
+}).call(this);
